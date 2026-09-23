f63fc43accdefdf55bdac47bbec87e910e90580d
Roman Viznij <ron.viz.dev@gmail.com>
2026-09-23 11:55:29 +0300

    small issue fix

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -18,6 +18,63 @@ const Cursor = () => {
     const requestRef = useRef(null);
 
     useEffect(() => {
+        const toggleCursorVisibility = () => {
+            if (cursorVisible.current) {
+                dot.current.style.opacity = 1;
+                dotOutline.current.style.opacity = 1;
+            } else {
+                dot.current.style.opacity = 0;
+                dotOutline.current.style.opacity = 0;
+            }
+        };
+
+        const toggleCursorSize = () => {
+            if (cursorEnlarged.current) {
+                dot.current.style.transform = 'translate(-50%, -50%) scale(0.75)';
+                dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1.5)';
+            } else {
+                dot.current.style.transform = 'translate(-50%, -50%) scale(1)';
+                dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1)';
+            }
+        };
+
+        const mouseOverEvent = () => {
+            cursorEnlarged.current = true;
+            toggleCursorSize();
+        };
+
+        const mouseOutEvent = () => {
+            cursorEnlarged.current = false;
+            toggleCursorSize();
+        };
+
+        const mouseEnterEvent = () => {
+            cursorVisible.current = true;
+            toggleCursorVisibility();
+        };
+
+        const mouseLeaveEvent = () => {
+            cursorVisible.current = false;
+            toggleCursorVisibility();
+        };
+
+        const mouseMoveEvent = e => {
+            cursorVisible.current = true;
+            toggleCursorVisibility();
+            endX.current = e.clientX;
+            endY.current = e.clientY;
+            dot.current.style.top = `${endY.current}px`;
+            dot.current.style.left = `${endX.current}px`;
+        };
+
+        const animateDotOutline = () => {
+            _x.current += (endX.current - _x.current) / delay;
+            _y.current += (endY.current - _y.current) / delay;
+            dotOutline.current.style.top = `${_y.current}px`;
+            dotOutline.current.style.left = `${_x.current}px`;
+            requestRef.current = requestAnimationFrame(animateDotOutline);
+        };
+
         document.addEventListener('mousedown', mouseOverEvent);
         document.addEventListener('mouseup', mouseOutEvent);
         document.addEventListener('mousemove', mouseMoveEvent);
@@ -36,67 +93,6 @@ const Cursor = () => {
             cancelAnimationFrame(requestRef.current);
         };
     }, []);
-
-    const toggleCursorVisibility = () => {
-        if (cursorVisible.current) {
-            dot.current.style.opacity = 1;
-            dotOutline.current.style.opacity = 1;
-        } else {
-            dot.current.style.opacity = 0;
-            dotOutline.current.style.opacity = 0;
-        }
-    };
-
-    const toggleCursorSize = () => {
-        if (cursorEnlarged.current) {
-            dot.current.style.transform = 'translate(-50%, -50%) scale(0.75)';
-            dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1.5)';
-        } else {
-            dot.current.style.transform = 'translate(-50%, -50%) scale(1)';
-            dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1)';
-        }
-    };
-
-    const mouseOverEvent = () => {
-        cursorEnlarged.current = true;
-        toggleCursorSize();
-    };
-
-    const mouseOutEvent = () => {
-        cursorEnlarged.current = false;
-        toggleCursorSize();
-    };
-
-    const mouseEnterEvent = () => {
-        cursorVisible.current = true;
-        toggleCursorVisibility();
-    };
-
-    const mouseLeaveEvent = () => {
-        cursorVisible.current = false;
-        toggleCursorVisibility();
-    };
-
-   const mouseMoveEvent = e => {
-        cursorVisible.current = true;
-        toggleCursorVisibility();
-
-        endX.current = e.clientX;
-        endY.current = e.clientY;
-
-        dot.current.style.top = endY.current + 'px';
-        dot.current.style.left = endX.current + 'px';
-    };
-
-    const animateDotOutline = () => {
-        _x.current += (endX.current - _x.current) / delay;
-        _y.current += (endY.current - _y.current) / delay;
-
-        dotOutline.current.style.top = _y.current + 'px';
-        dotOutline.current.style.left = _x.current + 'px';
-
-        requestRef.current = requestAnimationFrame(animateDotOutline);
-    };
 
     return (
         <>
